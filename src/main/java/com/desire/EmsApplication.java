d02493035ebf7abb378e2bb011ecf01cc3168ca1
hyzic <isaac.oghenevwapo@gmail.com>
2023-07-04 15:08:48 -0400

    Created Spring Boot Application and First Commit

---
+++ src/main/java/com/desire/EmsApplication.java
@@ -0,0 +1,13 @@
+package com.desire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmsApplication.class, args);
+	}
+
+}
